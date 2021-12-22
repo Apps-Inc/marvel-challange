cd89157f18f3aa019c69419f2473ce6e50ba7d2e
Diogo Neiss <diogo.oliveiran@gmail.com>
2021-12-22 16:25:53 -0300

    Adicionada collection view (lazy v grid)

--- MarvelChallenge/Pages/Home/HomeView.swift
+++ MarvelChallenge/Pages/Home/HomeView.swift
@@ -6,10 +6,58 @@ struct HomeView: View {
 
     @StateObject var viewModel = HomeViewModel()
 
+    @State var rowNumber = 1
+
+    /**
+     LazyVStack {
+     ForEach(0...5, id: \.self) {_ in
+     LazyHStack {
+     ForEach(0...3, id: \.self) { _ in
+     Rectangle()
+     .frame(width: 50, height: 50, alignment: .center)
+     .scaledToFill()
+     Spacer()
+     }
+     }
+     }
+     }
+     
+     */
+    /*
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ] */
+
+    var columns: [GridItem] {
+        var tmp = [GridItem]()
+
+        for _ in 1...rowNumber {
+            tmp.append(GridItem(.flexible()))
+        }
+
+        return tmp
+    }
+
+    /*
+    var columns: [GridItem] {
+        (1...rowNumber).map { GridItem(.flexible())  }
+    }
+     }
+     */
+
+    let data = (1...100).map { "Item \($0)" }
+
     var body: some View {
         NavigationView {
             ScrollView {
-                Text("Aqui entram as lazy stacks")
+                LazyVGrid(columns: columns, alignment: .center, spacing: 20) {
+                    ForEach(data, id: \.self) { item in
+                        Text(item)
+                    }
+                }
+
             }
         }
     }
@@ -20,7 +68,7 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
             .environment(\.managedObjectContext,
-             PersistenceController.preview.container.viewContext)
+                          PersistenceController.preview.container.viewContext)
             .previewDevice("iPhone 13")
     }
 }
