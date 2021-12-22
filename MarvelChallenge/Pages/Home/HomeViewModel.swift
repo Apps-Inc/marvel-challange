6a28234e960fd57641e0ac7fa35342f0c930baa6
Diogo Neiss <diogo.oliveiran@gmail.com>
2021-12-22 13:04:21 -0300

    Implementacao basica das viewmodels

--- MarvelChallenge/Pages/Home/HomeViewModel.swift
+++ MarvelChallenge/Pages/Home/HomeViewModel.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+protocol ViewModel {
+    func loadMore()
+
+    var marvelService: MarvelService { get }
+    var isLoading: Bool { get set}
+    //ver se chegou no pagina final pra parar de fazer request
+    var hasEnded: Bool { get set }
+    
+    //alertar qual foi o erro
+    var error: String { get set }
+    
+    //pagina atual
+    var currentPage: Int { get set }
+
+}
+
+class HomeViewModel: ViewModel, ObservableObject {
+
+    @Published var mainRequest: EventsMainRequest?
+    @Published var eventsList = [Event]()
+
+    internal var marvelService: MarvelService = MarvelServiceImp()
+    var currentPage: Int = 0
+    var isLoading: Bool = false
+
+    var hasEnded: Bool = false
+
+    var error: String = ""
+
+    func loadMore() {
+
+    }
+}
