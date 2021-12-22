0e1348bf740510dd3f6d0633659bbb7ea51bcb71
Diogo Neiss <diogo.oliveiran@gmail.com>
2021-12-22 13:19:08 -0300

    esqueleto de request na viewmodel

--- MarvelChallenge/Pages/Home/HomeViewModel.swift
+++ MarvelChallenge/Pages/Home/HomeViewModel.swift
@@ -12,14 +12,17 @@ protocol ViewModel {
 
     var marvelService: MarvelService { get }
     var isLoading: Bool { get set}
-    //ver se chegou no pagina final pra parar de fazer request
+    // ver se chegou no pagina final pra parar de fazer request
     var hasEnded: Bool { get set }
-    
-    //alertar qual foi o erro
+
+    // alertar qual foi o erro
     var error: String { get set }
-    
-    //pagina atual
+
+    // pagina atual
     var currentPage: Int { get set }
+
+    // quantas paginas existem
+    var pageTotal: Int {get set}
 
 }
 
@@ -36,7 +39,40 @@ class HomeViewModel: ViewModel, ObservableObject {
 
     var error: String = ""
 
+    // valor padrao, depois setamos pro valor correto
+    var pageTotal: Int = 1
+
+    private func getEvents(_ page: Int? = 1) {
+
+        marvelService.fetchEvents(limit: 10, offset: 0) { result in
+            /*
+             guard let result = result else {
+             self.error = "Erro na request"
+             return
+             }
+             */
+
+            self.isLoading = false
+
+            self.mainRequest = result
+            self.eventsList.append(contentsOf: result.data.results)
+        }
+
+    }
     func loadMore() {
+
+        if currentPage == pageTotal {
+            hasEnded = true
+            return
+        } else if isLoading {
+            return
+        }
+
+        isLoading = true
+
+        currentPage += 1
+
+        getEvents(currentPage)
 
     }
 }
