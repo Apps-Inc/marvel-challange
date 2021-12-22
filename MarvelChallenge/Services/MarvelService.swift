a07bc9e08c700d7e08c5fa8c284c0bd100a47e61
Victor Mello <victormmello@gmail.com>
2021-12-22 13:08:50 -0300

    marvel service refactor

--- MarvelChallenge/Services/MarvelService.swift
+++ MarvelChallenge/Services/MarvelService.swift
@@ -10,18 +10,17 @@ import CryptoKit
 import Alamofire
 
 protocol MarvelService {
-    typealias RequestType = (EventsMainRequest) -> Void
+// MARK: - EventsRequest
+    typealias EventsType = EventsMainRequest
+    typealias EventsHandler = (EventsType) -> Void
 
-    func fetchEvents(limit: Int,
-                     offset: Int,
-                     completionHandler: @escaping RequestType)
+    func fetchEvents(page: Int,
+                     completionHandler: @escaping EventsHandler)
 }
 
 class MarvelServiceImp: MarvelService {
 
-    static let LIMIT =  20
-    static let OFFSET = 0
-
+    private let limit = 20
     private let baseUrl = "https://gateway.marvel.com/v1/public"
     private let publicKey = "2dd999c3ed11ed12eb9a4fd38444b3a4"
     private let privateKey = "c67104a683dbfa5ef8e17114584ade00fed35f76"
@@ -33,47 +32,22 @@ class MarvelServiceImp: MarvelService {
         return bodyDecoder
     }()
 
-    private func fetchApi<T>(endpoint: String,
-                             parameters: [String: Any] = [:],
-                             type: T.Type = T.self,
-                             completionHandler: @escaping (AFDataResponse<T>) -> Void
-    ) where T: Decodable {
-        var parameters = parameters
-        let timeStamp = String(Date().hashValue)
-        let toHash = timeStamp + privateKey + publicKey
-        let hash = md5Hash(toHash)
-
-        parameters["ts"] = timeStamp
-        parameters["apikey"] = publicKey
-        parameters["hash"] = hash
-
-        print( " asdasd")
-
-        AF.request("\(baseUrl)\(endpoint)", parameters: parameters)
-            .validate(statusCode: 200..<300)
-            .response(completionHandler: { response in
-                debugPrint(response)
-            })
-            .responseDecodable(of: type,
-                               decoder: jsonDecoder,
-                               completionHandler: completionHandler)
-    }
-
-    func fetchEvents(limit: Int = LIMIT, offset: Int = OFFSET, completionHandler: @escaping RequestType ) {
-        let params = ["limit": limit, "offset": offset]
+// MARK: - public func
+    func fetchEvents(page: Int = 1, completionHandler: @escaping EventsHandler) {
+        let params = ["limit": limit, "offset": (page - 1) * limit]
 
         fetchApi(endpoint: "/events",
                  parameters: params,
-                 type: EventsMainRequest.self) { response in
+                 type: EventsType.self) { response in
 
             if let err = response.error {
-                print(" errr")
+                print("Request Error!")
                 print(err)
                 return
             }
 
             guard let data = response.value else {
-                print("null")
+                print("null data")
                 return
             }
 
@@ -81,7 +55,29 @@ class MarvelServiceImp: MarvelService {
         }
     }
 
-    func md5Hash(_ source: String) -> String {
+// MARK: - private func
+    private func fetchApi<T>(
+                        endpoint: String,
+                        parameters: [String: Any] = [:],
+                        type: T.Type = T.self,
+                        completionHandler: @escaping (AFDataResponse<T>) -> Void
+    ) where T: Decodable {
+        var parameters = parameters
+        let timeStamp = String(Date().hashValue)
+        let toHash = timeStamp + privateKey + publicKey
+
+        parameters["ts"] = timeStamp
+        parameters["apikey"] = publicKey
+        parameters["hash"] = md5Hash(toHash)
+
+        AF.request("\(baseUrl)\(endpoint)", parameters: parameters)
+            .validate(statusCode: 200..<300)
+            .responseDecodable(of: type,
+                               decoder: jsonDecoder,
+                               completionHandler: completionHandler)
+    }
+
+    private func md5Hash(_ source: String) -> String {
         return Insecure.MD5
             .hash(data: source.data(using: .utf8)!)
             .map { String(format: "%02hhx", $0) }
